510a03ec91b0d0790316303e1e19559f9fdcac41
AlexOdnov <alex.odnoralov@yandex.ru>
2021-04-21 23:05:51 +0300

    add CurrentWeather component

---
+++ src/components/CurrentWeather.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="current-weather">
+    <h1 class="current-weather__title">{{ cityName }}</h1>
+    <p class="current-weather__date">{{ currentDate }}</p>
+    <div class="current-weather__temperature"></div>
+    <div class="current-weather__wind"></div>
+    <div class="current-weather__pressure"></div>
+    <div class="current-weather__humidity"></div>
+    <div class="current-weather__precipitation"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CurrentWeather',
+
+  props: {
+    cityName: {
+      type: String,
+      required: true,
+    },
+    currentWeather: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    currentDate() {
+      const options = {
+        weekday: 'short',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+      };
+      return new Date(this.currentWeather.dt * 1000).toLocaleString(
+        'ru-ru',
+        options
+      );
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.current-weather {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  align-items: center;
+  padding: 15px;
+
+  @media (min-width: 480px) {
+    padding: 20px;
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+.current-weather__title {
+  grid-column: 1 / -1;
+  justify-self: start;
+  text-transform: capitalize;
+
+  @media (min-width: 480px) {
+    grid-column: 1 / 3;
+  }
+}
+.current-weather__date {
+  grid-column: 1 / -1;
+  font-size: 20px;
+  text-transform: capitalize;
+
+  @media (min-width: 480px) {
+    grid-column: -1 / -3;
+    justify-self: end;
+  }
+}
+</style>
--- src/views/City.vue
+++ src/views/City.vue
@@ -1,12 +1,21 @@
 <template>
-  <div></div>
+  <div class="forecast" v-if="forecast">
+    <current-weather
+      class="forecast__current"
+      :city-name="cityName"
+      :current-weather="forecast.current"
+    />
+  </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex';
+import CurrentWeather from '../components/CurrentWeather.vue';
 
 export default {
   name: 'City',
+
+  components: { CurrentWeather },
 
   data() {
     return {
@@ -31,4 +40,25 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.forecast {
+  display: grid;
+  grid-template-columns: 1fr;
+  max-width: 1440px;
+  margin: 0 auto;
+
+  @media (min-width: 480px) {
+    justify-items: center;
+  }
+  @media (min-width: 960px) {
+    grid-template-columns: auto 1fr;
+  }
+  @media (min-width: 1440px) {
+    grid-template-columns: 1fr 2fr;
+  }
+}
+.forecast__current {
+  grid-column: 1 / 2;
+  max-width: 480px;
+}
+</style>
